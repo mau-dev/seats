d1c6143b23cdee11b8b088e9fcf11ff6152a6786
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-09-20 15:39:21 +0800

    updated the js file

--- script.js
+++ script.js
@@ -7,34 +7,34 @@ var planeSeats = 10;
 var planeTicket = 50;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
+    console.log( currentInput );
 // totalPurchases += 1;
 
-var plane = {seats: 10,
-            ticket: 50,
-            // purchases:0
-            };
+// var plane = {seats: 10,
+//             ticket: 50,
+//             // purchases:0
+//             };
   //each time user types 'yes'
   // numberOfSeats decreses by 1
   //and,ticket price increases by 5% ticketPrice = ticketPrice * 1.05;
-if (currentInput === "yes") {
+    if (currentInput === "yes") {
     // plane.seats = plane.seats - 1;
     // plane.ticket = plane.ticket * 1.05;
-    planeSeats = planeSeats - 1;
-    planeTicket = planeTicket * 1.05;
+            planeSeats = planeSeats - 1;
+            planeTicket = planeTicket * 1.05;
 
      // plane.purchases = plane.purchases +1;
-     totalPurchases = totalPurchases +1;
-        var message =  totalPurchases + " tickets sold! There are  " + planeSeats + " seats left. New ticket price is " + planeTicket + ".";
+            totalPurchases = totalPurchases +1;
+             var message =  totalPurchases + " tickets sold! There are  " + planeSeats + " seats left. New ticket price is " + planeTicket + ".";
 
 
-} else {
-    message = "Hurry up! The price is raising with each ticket sold!"
-}
+    } else {
+             message = "Hurry up! The price is raising with each ticket sold!"
+    }
 
 
- display(message);
-
+ // display(message);
+ return message;
 
 
 }
